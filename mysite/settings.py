468ad73464fd86d625d16c0f3f2c2afc32b0436e
lpl19102000 <llai1@ualberta.ca>
2024-02-26 19:54:44 -0700

    allow Vercel to host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qwg!_@gn*&w6_i9jln(k=qj9uuc)*x@v*9&0ab^r-9-d*2j-ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
